33e00a19f8a114ee7cf9bcfbed3a9d78e62a3998
Robert Sharp <me@robertsharp.id.au>
2013-02-21 15:58:06 +1000

    Added test code
    Added test code to Main() to test proxypool with retries on & off

--- Test.cs
+++ Test.cs
@@ -9,8 +9,30 @@ namespace ProxyPool
     {
         static void Main()
         {
-            //initialise proxypool
-            ProxyPool.Getter proxyGetter = new Getter();
+            //create test proxy list
+            List<string> proxylist = new List<string>();
+            proxylist.Add("http://127.0.0.1:3128");
+
+            //create test URL list
+            List<string> urlList = new List<string>();
+
+            //currently using linode download test URLs
+            urlList.Add("http://speedtest.tokyo.linode.com/100MB-tokyo.bin");
+            urlList.Add("http://speedtest.london.linode.com/100MB-london.bin");
+            urlList.Add("http://speedtest.newark.linode.com/100MB-newark.bin");
+            urlList.Add("http://speedtest.atlanta.linode.com/100MB-atlanta.bin");
+            urlList.Add("http://speedtest.dallas.linode.com/100MB-dallas.bin");
+            urlList.Add("http://speedtest.fremont.linode.com/100MB-fremont.bin");
+
+            //initialise proxypool with retries off and test
+            Console.WriteLine("Testing ProxyPool with retries off");
+            ProxyPool.Getter proxyGetter = new Getter(proxylist, false);
+            proxyGetter.GetUrls(urlList.ToArray());
+
+            //re-initialise proxypool with retries set to default on and test
+            Console.WriteLine("Testing ProxyPool with retries on");
+            proxyGetter = new Getter(proxylist);
+            proxyGetter.GetUrls(urlList.ToArray());
         }
 
     }
